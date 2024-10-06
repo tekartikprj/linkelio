7fbd6e6677bf6640a8701996e099947731666a15
Alexandre Roux <alex@tekartik.com>
2024-10-06 23:42:37 +0200

    feat: add view and edit

--- packages/linkelio_common/lib/model/fs_link.dart
+++ packages/linkelio_common/lib/model/fs_link.dart
@@ -5,8 +5,20 @@ class FsLink extends CvFirestoreDocumentBase {
   /// url redirection
   final url = CvField<String>('url');
 
+  /// active status, default to true!
+  final active = CvField<bool>('active');
+
+  /// Timestamp
+  final created = CvField<Timestamp>('created');
+
+  /// User id
+  final userId = CvField<String>('userId');
+
+  /// get active status
+  bool get isActive => active.value ?? true;
+
   @override
-  CvFields get fields => [url];
+  CvFields get fields => [url, active, created, userId];
 }
 
 /// FsLink model (for queries)
