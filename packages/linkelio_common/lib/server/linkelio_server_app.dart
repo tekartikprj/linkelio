5482540b1ee56b88ec3041d8e78cd52c52c102fd
Alexandre Roux <alex@tekartik.com>
2024-10-12 16:14:30 +0200

    fix lints

--- packages/linkelio_common/lib/server/linkelio_server_app.dart
+++ packages/linkelio_common/lib/server/linkelio_server_app.dart
@@ -4,7 +4,6 @@ import 'package:linkelio_common/constants.dart';
 import 'package:linkelio_common/firestore/firestore_database.dart';
 import 'package:linkelio_common/html/html.dart';
 import 'package:linkelio_common/model/all_models.dart';
-import 'package:tekartik_app_http/app_http.dart';
 import 'package:tkcms_common/tkcms_common.dart';
 import 'package:tkcms_common/tkcms_flavor.dart';
 import 'package:tkcms_common/tkcms_server.dart';
